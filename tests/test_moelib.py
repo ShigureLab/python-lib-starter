86e1f3af04fa37a8de47da4cf1779f488a5d7af6
Nyakku Shigure <sigure.qaq@gmail.com>
2024-10-26 17:14:55 +0800

    🔧 chore: use PEP 735 dependency group (#61)

--- tests/test_moelib.py
+++ tests/test_moelib.py
@@ -1,15 +1,7 @@
 from __future__ import annotations
 
-from pathlib import Path
-
-# This can be replaced with tomllib if you are using Python 3.11+
-import tomli as tomllib
-
 from moelib import __version__
-
-with Path("pyproject.toml").open("rb") as f:
-    project_info = tomllib.load(f)
 
 
 def test_version():
-    assert __version__ == project_info["project"]["version"]
+    assert __version__ == "0.1.0"
